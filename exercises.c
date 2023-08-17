3817c375766acf590358896e8dff9104b2ff1121
Replit user <>
2023-08-17 01:05:10 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -9,7 +9,15 @@ Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size) { return 0; }
+int findMax(int arr[], int size){ 
+  int grande=0;
+  for (i=0;i>size;i++){
+    if (arr[i]>arr[i+1]){
+      grande=arr[i];
+    }
+  }
+  return grande;
+}
 
 /*
 Ejercicio 2: Invertir un Arreglo
